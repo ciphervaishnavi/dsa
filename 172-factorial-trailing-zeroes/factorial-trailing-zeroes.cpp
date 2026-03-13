7d005eff344ae27957e5958cd31128b8910321fa
vaishnavi shukla <vaishnavishukla439@gmail.com>
2026-03-13 22:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (1.80%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+       long long count=0;
+       while(n>0){
+        n=n/5;
+        count+=n;
+       }
+        return count;
+        
+    }
+};
