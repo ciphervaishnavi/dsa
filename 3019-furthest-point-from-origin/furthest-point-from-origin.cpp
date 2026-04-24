97ef80056902156d3a678decebf94b664c3edb39
vaishnavi shukla <vaishnavishukla439@gmail.com>
2026-04-24 21:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (65.27%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int l=0;
+        int r=0;
+        int d=0;
+        for(char &ch:moves){
+            if(ch=='L') l++;
+            else if(ch=='R') r++;
+            else d++;
+           
+        }
+         return abs(l-r)+d;
+        
+    }
+};
